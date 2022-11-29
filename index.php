ea657d9a48c7a5816ee2b64f9a5553ab40d79d87
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-29 17:33:37 +0100

    Add Function for Generate Password

--- index.php
+++ index.php
@@ -4,7 +4,37 @@
 // Milestone 2
 // Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
 
+//Lunghezza caratteri password
+$passwordLenght = $_GET['charactersNumber'];
+/* echo $passwordLenght; */
 
+//Funzione per generare la password
+function generatePassword($numb)
+{
+    //Elenco caratteri
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!$£%&*#@';
+    //Dichiariamo la password in uscita come un array
+    $password = array();
+    //Definiamo la lunghezza dei caratteri disponibili
+    $charactersLenght = strlen($characters);
+    /* var_dump($charactersLenght); */
+    //Cicliamo tot di volte in base al numero dato dall'utente
+    for ($i = 0; $i < $numb; $i++) {
+        //Generiamo un numero casuale che andrà da 0 al numero massimo di caratteri
+        $n = rand(0, $charactersLenght);
+        /* var_dump($n); */
+        //Inseriamo nel nostro array i caratteri in base ad un indice casuale
+        $password[] = $characters[$n];
+        /* var_dump($password); */
+    }
+    //Trasformiamo la password generata in una stringa
+    return implode($password);
+}
+
+//Password generata
+$passwordGenerated = generatePassword($passwordLenght);
+/* var_dump($passwordGenerated); */
+echo 'Ecco la tua password: ' . $passwordGenerated;
 
 
 ?>
@@ -25,9 +55,10 @@
     <h1>Password Generator</h1>
     <form action="index.php" method="get">
         <div>
-            <label for="password">Lunghezza password:</label>
-            <input name="password" id="password" type="number">
+            <label for="charactersNumber">Lunghezza caratteri password:</label>
+            <input name="charactersNumber" id="charactersNumber" type="number" placeholder="Inserisci un numero">
         </div>
+        <button type="submit">Genera password</button>
     </form>
 </body>
 
